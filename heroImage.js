dc6926510b901e1f245bdb506267a412ba92af79
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-11 10:26:00 +0900

    1

---
+++ heroImage.js
@@ -0,0 +1,70 @@
+
+var widthEntire = window.screen.width * window.devicePixelRatio;
+var widthContent = 0;
+var leftNum = 0;
+
+if (widthEntire > 700) {
+	widthContent = 700;
+} else {
+	widthContent = widthEntire;
+};
+
+leftNum = (widthEntire - widthContent) / 2;
+
+var body = document.querySelector("body");
+body.style.marginLeft = leftNum + "px";
+body.style.marginRight = leftNum + "px";
+
+
+addHero("#rank01");
+addHero("#rank02");
+
+function addHero(rankNum) {
+	var currentDiv = document.querySelector(rankNum);
+	currentDiv.style = "position: relative; height: 150px;"
+	
+	
+	var newBadge = document.createElement("div");
+	newBadge.style = "width:" + 80 + "px;height:" + 80 + "px; border-radius: 10%; background:#4af800; position: absolute; top:18px;  z-index: 9;"
+	currentDiv.appendChild(newBadge);
+
+	
+	var newImage = document.createElement("img");
+	newImage.src = "heroImages/hero01.jpg";
+	newImage.style = "border: 8px solid #170039; border-radius: 15%; width: 100px; height = 100px; position: absolute; top:0; left:50px; z-index: 10;";
+	
+	// belows don't work...
+	//newImage.style.boxShadow = "1px 1px 8px 50px #ffffff;";
+	currentDiv.appendChild(newImage);
+
+
+var www = 40;
+
+var Hero = {
+	ID: "who",
+	releaseNum: 0,
+	role: "Healer",
+	BG: {
+		BG01: {
+			win: 53.0,
+			play: 10.0,
+			ban: 10,
+			rmPoint: 0
+		}
+	}
+};
+
+
+	var newRect = document.createElement("div");
+	newRect.style = "width:" + Hero.BG.BG01.win * 10 + "px;height:" +  Hero.BG.BG01.play * 5 + "px; background:#1cd93c; position: absolute; left: 90px; top:18px;  z-index: 1;"
+	currentDiv.appendChild(newRect);
+	
+}
+
+
+
+
+
+
+
+
--- main.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-	var canvas = document.getElementById("canvas01");
-	var ctx = canvas.getContext("2d");
-	ctx.strokeRect(10,300,30,200);
-}
