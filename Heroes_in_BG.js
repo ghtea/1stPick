39e42c23c64d2864dacd0a1ddb70db638aef7bce
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-13 11:25:02 +0900

    Update Heroes_in_BG.js

--- Heroes_in_BG.js
+++ Heroes_in_BG.js
@@ -54,6 +54,21 @@ function rowCreate() {
     var tbl = document.querySelector('#tblGood');
     tbl.style.width = '100%';
     tbl.setAttribute('border', '1');
+    var tbdy = document.querySelector('#tbdyGood');
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = "NEW CELL1";
+}
+
+/*
+function rowCreate() {
+    var tbl = document.querySelector('#tblGood');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
     
     var tbdy = document.querySelector('#tbdyGood');
     var tr = document.createElement('tr');
@@ -82,6 +97,7 @@ function rowCreate() {
     tbl.appendChild(tbdy);
     body.appendChild(tbl)
 }
+*/
 
 /*
     var numSizeWin = 8;
