3d1757fddcc2cad1b538afbf6e1ddaa6f2dd5600
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-13 17:34:25 +0900

    Update Heroes_in_BG.js

--- Heroes_in_BG.js
+++ Heroes_in_BG.js
@@ -49,19 +49,22 @@ for (var i = 1; i <= entireRank; i++ ) {
 }
 */
 
+var test = rows[0];
+
 rowCreate();
 function rowCreate() {
     var tbl = document.querySelector('#tblGood');
     tbl.style.width = '100%';
     tbl.setAttribute('border', '1');
     var tbdy = document.querySelector('#tbdyGood');
-    var row = tbl.insertRow(0);
+    var row = tbl.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
-    cell1.innerHTML = "NEW CELL1";
+    cell1.innerHTML = "#1";
+    cell2.innerHTML=test;
 }
 
 /*
