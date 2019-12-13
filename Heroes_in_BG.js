7f8f1235fb85ab0458bb85edf3ee76a46d6bf28f
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-13 17:22:13 +0900

    Update Heroes_in_BG.js

--- Heroes_in_BG.js
+++ Heroes_in_BG.js
@@ -55,7 +55,7 @@ function rowCreate() {
     tbl.style.width = '100%';
     tbl.setAttribute('border', '1');
     var tbdy = document.querySelector('#tbdyGood');
-    var row = table.insertRow(0);
+    var row = tbl.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
