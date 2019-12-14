e9d82b58493e04feede8e8136e6e4aae1dec480a
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-14 11:48:08 +0900

    1

--- Heroes_in_BG.js
+++ Heroes_in_BG.js
@@ -70,7 +70,7 @@ function rowCreate() {
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
     cell1.innerHTML = "#1";
-    cell2.innerHTML= "test";
+    cell2.innerHTML= "<img src=" + "heroImages/hero02.jpg" + ">";
 };
 
 /*
