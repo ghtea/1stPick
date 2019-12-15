0f555e903ea4b7f4dfff511e2db697b3c438b6b5
ghtea <47841931+ghtea@users.noreply.github.com>
2019-12-16 06:51:17 +0900

    Update Heroes_in_BG.js

--- Heroes_in_BG.js
+++ Heroes_in_BG.js
@@ -79,8 +79,8 @@ function updatePage (){
         var RectWidth = (dataSliced[i]['WinRate'] - 40) * numSizeWin;
         var RectHeight = (dataSliced[i]['PlayRate'] + dataSliced[i]['BanRate']) * numSizePlay;
         Rect.style = "width:" + RectWidth + "px;height:" +  RectHeight + "px;" ;
-        Rect.style.background =  "rgb(129,250,177);" ;
-        Rect.style.background =   "linear-gradient(128deg, rgba(129,250,177,1) 0%, rgba(59,230,20,1) 50%, rgba(59,230,20,1) 100%);" ;
+        Rect.style.backgroundColor =  "rgb(129,250,177);" ;
+        Rect.style.backgroundColor =   "linear-gradient(128deg, rgba(129,250,177,1) 0%, rgba(59,230,20,1) 50%, rgba(59,230,20,1) 100%);" ;
         cell3.appendChild(Rect);
     
         var txtGames = (100/dataSliced[i]['PlayRate']).toFixed(1);
