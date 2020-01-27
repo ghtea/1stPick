73a46d3fabe4bf75e53884ab529838f3f4ed4e2b
ghtea <47841931+ghtea@users.noreply.github.com>
2020-01-28 04:42:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -152,12 +152,12 @@ function updatePage() {
         roleInitial = "S";
         break;
     }
-    var divRole = document.createElement("div");
+    var divRoleTd = document.createElement("div");
     var rectRole = document.createElement("div");
     rectRole.setAttribute("class", "role" + dataSliced[i]["Role"]);
     rectRole.innerHTML = roleInitial;
+    divRoleTd.appendChild(rectRole);
     cell3.appendChild(divRole);
-    divRole.appendChild(rectRole);
 
     for (var k = 0; k < parseInt(dataSliced[i]["Difficulty"]); k++) {
       var rectDifficulty = [];
