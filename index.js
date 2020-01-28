44071d5ba8b6ae8b5bdf57e5656b782bdab5f962
ghtea <47841931+ghtea@users.noreply.github.com>
2020-01-28 21:12:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ function hideSome() {
 function checkSome() {
   var rows = document.querySelectorAll("#tableMain tr");
   for (var rowNum = 1; rowNum < numHero + 1; rowNum++) {
-    var currentrow = rows[rowNum + 1];
+    var currentrow = rows[rowNum];
     var checkbox = document.querySelector(
       ".cbxPerHero:nth-child(" + rowNum + ")"
     );
